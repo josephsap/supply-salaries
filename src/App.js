058616bde600b3029f17950a937ecfff8506a2c0
josephsap <joesap1@gmail.com>
2018-11-01 22:18:17 -0600

    api all route, cors err

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+
+  // http://events.thesupply.com/api/salaries/developer/seattle
+
+  state = {
+    jobTitles: [],
+    cities: []
+  }
+
+  componentDidMount() {
+    axios.get('http://events.thesupply.com/api/salaries/developer/seattle')
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <select>
+          <option value="designer">Designer</option>
+          <option value="developer">Developer</option>
+        </select>
+        <select>
+          <option value="charlotte">Charlotte</option>
+          <option value="seattle">Seattle</option>
+        </select>
       </div>
     );
   }
